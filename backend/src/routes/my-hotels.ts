3b0a5f755b6555976ff546fe0d11907257c7994b
Your Name <vivekundirwade@gmail.com>
2024-08-21 11:05:31 +0530

    addded my hotels view and added e2e tests

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express"
 import multer from 'multer'
 import cloudinary from "cloudinary"
-import Hotel, { HotelType } from "../models/hotel";
+import Hotel from "../models/hotel";
+import { HotelType } from "../shared/types";
 import verifyToken from "../middleware/auth";
 import { body, validationResult } from "express-validator";
 const myHotelRoutes = express.Router()
@@ -65,6 +66,17 @@ myHotelRoutes.post("/",
 
         }
     })
+
+//to see the hotels added by the user 
+myHotelRoutes.get("/", verifyToken, async(req:Request, res: Response)=>{
+   
+    try {
+        const hotels = await Hotel.find({userId: req.userId})
+        res.json(hotels);
+    } catch (error) {
+        res.status(500).json({message: "Error fetching hotels"})
+    }
+})
 
 
 async function uploadImages(imageFiles: Express.Multer.File[]) {
